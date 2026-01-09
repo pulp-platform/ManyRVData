2e20d2523d129e48938733aab8e8de9756faaa92
Diyou Shen <dishen@iis.ee.ethz.ch>
2026-01-09 12:30:58 +0100

    [SRC] Change hardware barrier to the new two-level hardware barrier for cachepool.

---
+++ sim/scripts/vsim_cluster.tcl
@@ -0,0 +1,19 @@
+# Copyright 2026 ETH Zurich and University of Bologna.
+# Solderpad Hardware License, Version 0.51, see LICENSE for details.
+# SPDX-License-Identifier: SHL-0.51
+
+# Create group for Cluster
+onerror {resume}
+
+set cluster_path $1
+
+add wave -noupdate -group Cluster -group xbar -group req_xbar ${cluster_path}/i_cluster_xbar/i_req_xbar/*
+add wave -noupdate -group Cluster -group xbar -group rsp_xbar ${cluster_path}/i_cluster_xbar/i_rsp_xbar/*
+add wave -noupdate -group Cluster -group xbar ${cluster_path}/i_cluster_xbar/*
+
+add wave -noupdate -group Cluster -group CSR ${cluster_path}/i_cachepool_cluster_peripheral/*
+
+add wave -noupdate -group Cluster -group Internal ${cluster_path}/*
+
+add wave -noupdate -group Barrier -group Global ${cluster_path}/i_cachepool_cluster_barrier/*
+
