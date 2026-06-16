e6f1a1eb21b3e9bfbe11b8912ab9325d164b7c2f
Diyou Shen <dishen@iis.ee.ethz.ch>
2026-06-16 16:46:06 +0200

    [MISC] Clean up Makefile and change the test size for load-store test.

--- util/auto-benchmark/configs.sh
+++ util/auto-benchmark/configs.sh
@@ -1,6 +1,6 @@
 # Configs and kernel suffixes (without prefix)
 
-CONFIGS="cachepool_fpu_2g cachepool_fpu_4g"
+CONFIGS="cachepool_fpu_2g cachepool_fpu_16g"
 
 KERNELS="spin-lock fdotp-32b_M65536 gemv-opt_M1024_N128_K32 gemv_M1024_N128_K32 fmatmul-32b_M64_N64_K64 multi_producer_single_consumer_double_linked_list_M1_N1350_K100 byte-enable"
 
